4e24d27a6dcb1f1537f779787efcda36a670a63a
Gustavo <gustavopereirafacal@gmail.com>
2022-11-18 11:58:52 -0300

    imagens salvas na pagina do talhado

--- src/Components/Galley/index.tsx
+++ src/Components/Galley/index.tsx
@@ -2,7 +2,8 @@ import Button from 'Components/Button'
 import { useContext, useState } from 'react'
 import { CarrouselContext } from 'Common/Carrousel.d'
 import { AiFillCloseCircle } from 'react-icons/ai'
-import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
+import { ImagensSlide } from 'Mock/Galery'
 import * as C from './style'
 
 type ICards = typeof Cards[0]
@@ -11,128 +12,75 @@ const Cards = [
     {
         id: 1,
         img: 'Assets/Sallers/MadeiraCambara.jpg',
-        title: "Vigamento Cambará"  
+        title: "Vigamento Cambará"
     },
 
     {
         id: 2,
         img: 'Assets/Sallers/TelhaOndulada.jpg',
-        title: "Telha cerâmica"  
+        title: "Telha cerâmica"
     },
 
     {
         id: 3,
         img: 'Assets/Sallers/TelhaFibrocimento.jpg',
-        title: "Telha ondulada"  
+        title: "Telha ondulada"
     },
 
     {
         id: 4,
         img: 'Assets/Sallers/ForroSerrado.jpg',
-        title: "Forro De Madeira"  
+        title: "Forro De Madeira"
     }
 ]
 
 type IImagensSlide = typeof ImagensSlide[0]
 
-const ImagensSlide = [
-    {
-        id: 1,
-        title: "",
-        img: 'Assets/Slide/Canteiro.png',     
-        category: 1
-    },
-
-    {
-        id: 2,
-        title: "",
-        img: 'Assets/Slide/Canteiro.png',
-        category: 2
-    },
-
-    {
-        id: 3,
-        title: "",
-        img: 'Assets/Slide/Canteiro.png',
-        category: 3
-    },
-
-    {
-        id: 4,
-        title: "",
-        img: 'Assets/Slide/Canteiro.png',
-        category: 4
-    },
-
-    {
-        id: 5,
-        title: "dasdasdasd",
-        img: 'Assets/Slide/Canteiro3.png',
-        category: 4
-    },
-
-    {
-        id: 6,
-        title: "dasdasdasd",
-        img: 'Assets/Slide/Canteiro3.png',
-        category: 4
-    },
-
-    {
-        id: 7,
-        title: "dasdasdasd",
-        img: 'Assets/Slide/Canteiro3.png',
-        category: 2
-    },
-
-    
-]
-
 const Gallery = () => {
 
-     const slideShow = useContext(CarrouselContext)
-    
-    const [active, setActive] = useState< number | boolean | null | ICards>(null)
+    const slideShow = useContext(CarrouselContext)
+
+    const [active, setActive] = useState<number | boolean | null | ICards>(null)
 
     const FilteredIndex = (index: ICards) => {
-        if(active !== index){
+        if (active !== index) {
             setActive(index.id)
-        }else {
+        } else {
             setActive(null)
-    
+
         }
     }
 
-    const filterSalles = active ? 
-    ImagensSlide.filter((item: any) =>  item.category === active) : []
+    const filterSalles = active ?
+        ImagensSlide.filter((item: any) => item.category === active) : []
 
-    return(
+    return (
         <C.Container>
             <C.Box >
-                {Cards.map((item) => 
+                {Cards.map((item) =>
                     <C.CardsInfo  >
-                        <img 
-                        onClick={() => FilteredIndex(item)} 
-                        key={item.id}
-                        src={item.img} alt={item.title} />
+                        <img
+                            onClick={() => FilteredIndex(item)}
+                            key={item.id}
+                            src={item.img} alt="" />
                         <p>{item.title}</p>
                     </C.CardsInfo>
                 )}
-            </C.Box>  
+            </C.Box>
 
             {active ? (
                 <C.PoupUp>
                     <C.CardPoupUp ref={slideShow?.carrousel}>
                         {filterSalles.map((item: IImagensSlide) => (
                             <C.BoxImg key={item.id}>
-                                <img src={item.img} alt={item.title} />  
+                                <img src={item.img} alt="" />
                             </C.BoxImg>
                         ))}
                     </C.CardPoupUp>
                     <C.Actions>
                         <span onClick={() => setActive(false)}><AiFillCloseCircle /></span>
-                        <Button type='button' onClick={slideShow?.handleLeftClick}><FaArrowAltCircleLeft/></Button>
-                        <Button type='button' onClick={slideShow?.handleRightClick}><FaArrowAltCircleRight/></Button>
+                        <Button type='button' onClick={slideShow?.handleLeftClick}><FaArrowAltCircleLeft /></Button>
+                        <Button type='button' onClick={slideShow?.handleRightClick}><FaArrowAltCircleRight /></Button>
                     </C.Actions>
                 </C.PoupUp>
             ) : <></>}
---
+++ src/Mock/Galery.ts
@@ -0,0 +1,233 @@
+export const ImagensSlide = [
+    {
+        id: 1,
+        img: 'Assets/Slide/Cambara/1.jpg',
+        category: 1
+    },
+
+    {
+        id: 2,
+        img: 'Assets/Slide/Cambara/2.jpg',
+        category: 1
+    },
+
+    {
+        id: 3,
+        img: 'Assets/Slide/Cambara/3.jpg',
+        category: 1
+    },
+
+    {
+        id: 4,
+        img: 'Assets/Slide/Cambara/4.jpg',
+        category: 1
+    },
+
+    {
+        id: 5,
+        img: 'Assets/Slide/Cambara/5.jpg',
+        category: 1
+    },
+
+    {
+        id: 6,
+        img: 'Assets/Slide/Cambara/6.jpg',
+        category: 1
+    },
+
+    {
+        id: 7,
+        img: 'Assets/Slide/Cambara/7.jpg',
+        category: 1
+    },
+
+    {
+        id: 8,
+        img: 'Assets/Slide/Cambara/8.jpg',
+        category: 1
+    },
+
+
+    {
+        id: 9,
+        img: 'Assets/Slide/Cambara/9.jpg',
+        category: 1
+    },
+
+    {
+        id: 10,
+        img: 'Assets/Slide/Cambara/10.jpg',
+        category: 1
+    },
+    {
+        id: 11,
+        img: 'Assets/Slide/Cambara/11.jpg',
+        category: 1
+    },
+
+    {
+        id: 12,
+        img: 'Assets/Slide/Ceramica/1.jpg',
+        category: 1
+    },
+
+    {
+        id: 13,
+        img: 'Assets/Slide/Ceramica/2.jpg',
+        category: 2
+    },
+
+    {
+        id: 14,
+        img: 'Assets/Slide/Ceramica/3.jpg',
+        category: 2
+    },
+    {
+        id: 15,
+        img: 'Assets/Slide/Ceramica/4.jpg',
+        category: 2
+    },
+    {
+        id: 16,
+        img: 'Assets/Slide/Ceramica/5.jpg',
+        category: 2
+    },
+    {
+        id: 17,
+        img: 'Assets/Slide/Ceramica/6.jpg',
+        category: 2
+    },
+    {
+        id: 18,
+        img: 'Assets/Slide/Ceramica/7.jpg',
+        category: 2
+    },
+    {
+        id: 19,
+        img: 'Assets/Slide/Ceramica/8.jpg',
+        category: 2
+    },
+    {
+        id: 20,
+        img: 'Assets/Slide/Ceramica/9.jpg',
+        category: 2
+    },
+    {
+        id: 21,
+        img: 'Assets/Slide/Ceramica/10.jpg',
+        category: 2
+    },
+    {
+        id: 22,
+        img: 'Assets/Slide/Ceramica/11.jpg',
+        category: 2
+    },
+    {
+        id: 23,
+        img: 'Assets/Slide/Telha/1.jpg',
+        category: 3
+    },
+    {
+        id: 24,
+        img: 'Assets/Slide/Telha/2.jpg',
+        category: 3
+    },
+
+    {
+        id: 25,
+        img: 'Assets/Slide/Telha/3.jpg',
+        category: 3
+    },
+    {
+        id: 26,
+        img: 'Assets/Slide/Telha/4.jpg',
+        category: 3
+    },
+
+    {
+        id: 27,
+        img: 'Assets/Slide/Telha/5.jpg',
+        category: 3
+    },
+    {
+        id: 28,
+        img: 'Assets/Slide/Telha/6.jpg',
+        category: 3
+    },
+
+    {
+        id: 29,
+        img: 'Assets/Slide/Telha/7.jpg',
+        category: 3
+    },
+
+    {
+        id: 30,
+        img: 'Assets/Slide/Telha/8.jpg',
+        category: 3
+    },
+
+    {
+        id: 31,
+        img: 'Assets/Slide/Forro/imagem 1.jpg',
+        category: 4
+    },
+
+    {
+        id: 32,
+        img: 'Assets/Slide/Forro/imagem 2.jpg',
+        category: 4
+    },
+
+    {
+        id: 33,
+        img: 'Assets/Slide/Forro/imagem 3.jpg',
+        category: 4
+    },
+    {
+        id: 34,
+        img: 'Assets/Slide/Forro/imagem 4.jpg',
+        category: 4
+    },
+    {
+        id: 35,
+        img: 'Assets/Slide/Forro/imagem 5.jpg',
+        category: 4
+    },
+    {
+        id: 36,
+        img: 'Assets/Slide/Forro/imagem 6.jpg',
+        category: 4
+    },
+    {
+        id: 37,
+        img: 'Assets/Slide/Forro/imagem 7.jpg',
+        category: 4
+    },
+    {
+        id: 38,
+        img: 'Assets/Slide/Forro/imagem 8.jpg',
+        category: 4
+    },
+    {
+        id: 39,
+        img: 'Assets/Slide/Forro/imagem 9.jpg',
+        category: 4
+    },
+    {
+        id: 40,
+        img: 'Assets/Slide/Forro/imagem 10.jpg',
+        category: 4
+    },
+    {
+        id: 41,
+        img: 'Assets/Slide/Forro/imagem 11.jpg',
+        category: 4
+    },
+
+    {
+        id: 42,
+        img: 'Assets/Slide/Forro/imagem 1.jpg',
+        category: 4
+    },
+]
